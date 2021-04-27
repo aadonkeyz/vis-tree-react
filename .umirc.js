db97132ab7c14f1a5bd8600e5bc51b651fad79f3
aadonkeyz <aadonkeyz@gmail.com>
2021-04-27 23:22:09 +0800

    feat: init

---
+++ .umirc.js
@@ -0,0 +1,15 @@
+export default {
+  mode: "doc",
+  title: "vis-tree-react",
+  logo: ".",
+  locales: [
+    ["zh-CN", "中文"],
+    ["en-US", "English"],
+  ],
+  resolve: {
+    includes: ["src"],
+  },
+  base: "/vis-tree-react",
+  publicPath: "/vis-tree-react/",
+  exportStatic: {},
+};
